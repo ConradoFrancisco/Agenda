bec79b69649e97234d021f3657f8e3f445805a6a
Conrado Llanos <collanos@legislaturacaba.ar>
2024-01-03 17:55:22 -0300

    Agregado los dias y otras cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-
-import { useState } from 'react';
 import './App.css'
-import moment from 'moment'
-
-function Calendar(){
-  const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const [month,setMonth] = useState(2);
-  const date = moment(`2024-0${month}-01`);
-  
-  const days = date.daysInMonth();
-  const day = date.day()
-  const dates = Array(35).fill(null)
-
-console.log(date);
-  return(
-  <>
-  <h2>{MONTHS[month-1]}</h2>
-    <div className='calendar'>
-      {dates.map((_,key)=>{
-        return(<div key={key}>{key + 1 > day && key + 1 - day <= days ? key + 1 - day : ''}</div>)
-      })}
-    </div>
-    <div style={{display:'flex'}}>
-      <button onClick={() => setMonth(prevState => prevState-1)} > - </button>
-      <button onClick={() => setMonth(prevState => prevState+1)}> + </button>
-    </div>
-  </>
-  )
-}
-
+import { Calendar } from './components/Calendar';
+import '../public/fecha.css'
+import 'react-responsive-modal/styles.css';
 function App() {
  
 
@@ -39,7 +12,6 @@ function App() {
           <h1>Agenda 2024</h1>
         </header>
         <Calendar/>
-
       </main>
     </>
   )
